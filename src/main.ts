954bbde5c87be6650757db6f0e1525a912c34dd4
hoang.phan <hoang.phan@advesa.com>
2023-03-25 18:26:07 +0700

    open cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
   app.use(cookieParser());
+  app.enableCors();
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
